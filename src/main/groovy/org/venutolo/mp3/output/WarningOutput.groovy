fefe78e260abe83ad1755c69eeb822c9509a467b
Rick Venutolo <rick@venutolo.org>
2020-12-31 14:55:37 -0600

    Change WarningOutput from interface to class

--- src/main/groovy/org/venutolo/mp3/output/WarningOutput.groovy
+++ src/main/groovy/org/venutolo/mp3/output/WarningOutput.groovy
@@ -3,14 +3,22 @@ package org.venutolo.mp3.output
 import javax.annotation.Nonnull
 import org.jaudiotagger.audio.mp3.MP3File
 
-interface WarningOutput {
+class WarningOutput {
 
-    void write(@Nonnull final MP3File file, @Nonnull final String message)
+    void write(@Nonnull final MP3File mp3File, @Nonnull final String message) {
+        write(mp3File.file, message)
+    }
 
-    void write(@Nonnull final MP3File file, @Nonnull final String message, @Nonnull final String details)
+    void write(@Nonnull final MP3File mp3File, @Nonnull final String message, @Nonnull final String details) {
+        write(mp3File.file, message, details)
+    }
 
-    void write(@Nonnull final File file, @Nonnull final String message)
+    void write(@Nonnull final File file, @Nonnull final String message) {
+        println("${file.canonicalPath}: ${message}")
+    }
 
-    void write(@Nonnull final File file, @Nonnull final String message, @Nonnull final String details)
+    void write(@Nonnull final File file, @Nonnull final String message, @Nonnull final String details) {
+        println("${file.canonicalPath}: ${message} (${details})")
+    }
 
 }
