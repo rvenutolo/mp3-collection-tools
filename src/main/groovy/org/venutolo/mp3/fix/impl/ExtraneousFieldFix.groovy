455cf1073176a4cff2b9ebab7e33cf6a70e24563
Rick Venutolo <rick@venutolo.org>
2021-01-18 16:04:56 -0600

    Extraneous fields fix

---
+++ src/main/groovy/org/venutolo/mp3/fix/impl/ExtraneousFieldFix.groovy
@@ -0,0 +1,34 @@
+package org.venutolo.mp3.fix.impl
+
+import static org.venutolo.mp3.Constants.EXTRANEOUS_FIELDS
+
+import groovy.util.logging.Slf4j
+import javax.annotation.Nonnull
+import org.jaudiotagger.audio.mp3.MP3File
+import org.venutolo.mp3.fix.AbstractMp3FileFix
+import org.venutolo.mp3.output.Output
+
+@Slf4j
+class ExtraneousFieldFix extends AbstractMp3FileFix {
+
+    ExtraneousFieldFix(@Nonnull final Output output) {
+        super(log, output, true)
+    }
+
+    @Override
+    boolean fixInternal(@Nonnull final MP3File mp3File) {
+        def fixed = false
+        def tag = mp3File.getID3v2Tag()
+        EXTRANEOUS_FIELDS.each { field ->
+            def fieldValue = tag.getFirst(field.key)
+            if (fieldValue) {
+                log.debug('Removing {} field: {}', field.desc, fieldValue)
+                tag.deleteField(field.key)
+                output.write(mp3File, "Removed: ${field.desc}")
+                fixed = true
+            }
+        }
+        fixed
+    }
+
+}
