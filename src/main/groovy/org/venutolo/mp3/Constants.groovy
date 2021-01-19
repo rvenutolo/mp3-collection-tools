310e9d7a7b4a9ea80444f657ead6307e09ae927d
Rick Venutolo <rick@venutolo.org>
2021-01-19 16:37:59 -0600

    Remove unused constants

--- src/main/groovy/org/venutolo/mp3/Constants.groovy
+++ src/main/groovy/org/venutolo/mp3/Constants.groovy
@@ -14,8 +14,6 @@ class Constants {
     public static final int ID3V2_TARGET_MAJOR_VERSION = 4
     public static final String ALBUM_IMAGE_FILENAME = 'Folder.jpg'
     public static final int TARGET_PIXELS = 1000
-    public static final Pattern TWO_DIGITS = ~$/\d{2}/$
-    public static final Pattern THREE_DIGITS = ~$/\d{3}/$
     public static final Pattern FOUR_DIGITS = ~$/\d{4}/$
     public static final Collection<Field> REQUIRED_FIELDS = Field.values().findAll { it.required }
     public static final Collection<Field> EXTRANEOUS_FIELDS = Field.values().findAll { !it.required }
