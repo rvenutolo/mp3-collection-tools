38177f73281300b32e1c786c8112c0b1bc269ee5
Rick Venutolo <rick@venutolo.org>
2021-03-23 21:46:24 -0400

    Remove unused constant

--- src/main/groovy/org/venutolo/mp3/core/Constants.groovy
+++ src/main/groovy/org/venutolo/mp3/core/Constants.groovy
@@ -8,7 +8,7 @@ import static Field.YEAR
 import static org.venutolo.mp3.core.Id3v2Tag.Version.V2_4
 
 import java.util.regex.Pattern
-import static org.venutolo.mp3.core.Id3v2Tag.Version
+import org.venutolo.mp3.core.Id3v2Tag.Version
 
 class Constants {
 
@@ -18,9 +18,9 @@ class Constants {
     public static final int TARGET_PIXELS = 1000
     public static final Pattern FOUR_DIGITS = ~$/\d{4}/$
     public static final Pattern FULL_DATE = ~$/\d{4}-\d{2}-\d{2}/$
+    // TODO remove these field collections? - check where used
     public static final Collection<Field> REQUIRED_FIELDS = Field.values().findAll { field -> field.isRequired }
     public static final Collection<Field> EXTRANEOUS_FIELDS = Field.values().findAll { field -> !field.isRequired }
-    public static final Collection<Field> NUMERIC_FIELDS = Field.values().findAll { field -> field.isNumeric }
     public static final Collection<Field> SAME_VALUE_FIELDS = [ALBUM_ARTIST, ALBUM, GENRE, YEAR, TRACK_TOTAL]
     public static final Set<String> ALLOWED_GENRES = [
         'Alternative Metal',
